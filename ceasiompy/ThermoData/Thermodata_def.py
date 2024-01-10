f42fc2b31596c87977da001df170af0e53a580d6
Francesco-Marcucci <fra.marcuc@libero.it>
2024-01-10 11:41:32 +0100

    new commit

---
+++ ceasiompy/ThermoData/Thermodata_def.py
@@ -0,0 +1,100 @@
+"""
+CEASIOMpy: Conceptual Aircraft Design Software
+
+Developed by CFS ENGINEERING, 1015 Lausanne, Switzerland
+
+Calculate outlet conditions fot turbojet and turbofan engines
+
+| Author: Francesco Marcucci
+| Creation: 2023
+"""
+
+# =================================================================================================
+#   IMPORTS
+# =================================================================================================
+
+from pathlib import Path
+
+from ceasiompy.ThermoData.func.des_hbtf_func_test_2 import run_turbofan_analysis_test_2
+
+from ceasiompy.ThermoData.func.des_turbojet_func import run_turbojet_analysis
+
+from ceasiompy.utils.ceasiomlogger import get_logger
+from ceasiompy.utils.moduleinterfaces import (
+    check_cpacs_input_requirements,
+    get_toolinput_file_path,
+    get_tooloutput_file_path,
+)
+from ceasiompy.utils.commonxpath import (
+    REF_XPATH,
+    CLCALC_XPATH,
+)
+from cpacspy.cpacsfunctions import (
+    get_value_or_default,
+)
+from cpacspy.cpacsfunctions import create_branch, get_value, get_value_or_default
+from cpacspy.cpacspy import CPACS
+from markdownpy.markdownpy import MarkdownDoc
+
+log = get_logger()
+
+MODULE_DIR = Path(__file__).parent
+MODULE_NAME = MODULE_DIR.name
+
+
+# =================================================================================================
+#   CLASSES
+# =================================================================================================
+
+
+# =================================================================================================
+#   FUNCTIONS
+# =================================================================================================
+
+
+# =================================================================================================
+#    MAIN
+# =================================================================================================
+
+# XPath definition
+
+ref_area_xpath = REF_XPATH + "/area"
+
+mass_type_xpath = CLCALC_XPATH + "/massType"
+
+custom_mass_xpath = CLCALC_XPATH + "/customMass"
+
+percent_fuel_mass_xpath = CLCALC_XPATH + "/percentFuelMass"
+
+cruise_alt_xpath = CLCALC_XPATH + "/cruiseAltitude"
+
+cruise_mach_xpath = CLCALC_XPATH + "/cruiseMach"
+
+load_fact_xpath = CLCALC_XPATH + "/loadFactor"
+
+
+def main(cpacs_path, cpacs_out_path):
+
+    log.info("----- Start of " + MODULE_NAME + " -----")
+    cpacs = CPACS(cpacs_path)
+    tixi = cpacs.tixi
+
+    alt = get_value_or_default(tixi, cruise_alt_xpath, 1000)
+
+    MN = get_value_or_default(tixi, cruise_mach_xpath, 0.3)
+
+    Fn = 2000
+
+    run_turbojet_analysis(alt, MN, Fn)
+
+    run_turbofan_analysis_test_2(alt, MN, Fn)
+
+    log.info("----- End of " + MODULE_NAME + " -----")
+
+
+if __name__ == "__main__":
+
+    cpacs_path = get_toolinput_file_path(MODULE_NAME)
+    cpacs_out_path = get_tooloutput_file_path(MODULE_NAME)
+
+    main(cpacs_path, cpacs_out_path)
--- ceasiompy/ThermoData/__specs__.py
+++ ceasiompy/ThermoData/__specs__.py
@@ -1,63 +1,79 @@
+from pathlib import Path
 from ceasiompy.utils.moduleinterfaces import CPACSInOut
-from ceasiompy.utils.commonxpath import CEASIOMPY_XPATH, FUSELAGES_XPATH
+from ceasiompy.utils.commonxpath import (
+    REF_XPATH,
+    CLCALC_XPATH,
+    SU2_FIXED_CL_XPATH,
+    SU2_TARGET_CL_XPATH,
+)
 
 # ===== Module Status =====
 # True if the module is active
 # False if the module is disabled (not working or not ready)
-module_status = False  # Because it is just an example not a real module
+module_status = True
+
+# ===== Results directory path =====
+
+RESULTS_DIR = Path("Results", "Thermodata")
 
 # ===== CPACS inputs and outputs =====
 
 cpacs_inout = CPACSInOut()
 
-include_gui = False
+# ===== Input =====
 
-# ----- Input -----
-
-# * In the following example we add three (!) new entries to 'cpacs_inout'
-# * Try to use (readable) loops instead of copy-pasting three almost same entries :)
-for direction in ["x", "y", "z"]:
-    cpacs_inout.add_input(
-        var_name=direction,
-        var_type=float,
-        default_value=None,
-        unit="1",
-        descr=f"Fuselage scaling on {direction} axis",
-        xpath=FUSELAGES_XPATH + f"/fuselage/transformation/scaling/{direction}",
-        gui=include_gui,
-        gui_name=f"{direction.capitalize()} scaling",
-        gui_group="Fuselage scaling",
-    )
 
 cpacs_inout.add_input(
-    var_name="test",
-    var_type=str,
-    default_value="This is a test",
-    unit=None,
-    descr="This is a test of description",
-    xpath=CEASIOMPY_XPATH + "/test/myTest",
-    gui=include_gui,
-    gui_name="My test",
-    gui_group="Group Test",
+    var_name="cruise_mach",
+    var_type=float,
+    default_value=0.3,
+    unit="1",
+    descr="Aircraft cruise Mach number",
+    xpath=CLCALC_XPATH + "/cruiseMach",
+    gui=True,
+    gui_name="Mach",
+    gui_group="Cruise",
 )
 
 cpacs_inout.add_input(
-    var_name="other_var",
-    var_type=list,
-    default_value=[2, 33, 444],
-    unit="[unit]",
-    xpath=CEASIOMPY_XPATH + "/test/myList",
-    gui=include_gui,
-    gui_name="Choice",
-    gui_group="My Selection",
+    var_name="cruise_alt",
+    var_type=float,
+    default_value=1000.0,
+    unit="m",
+    descr="Aircraft cruise altitude",
+    xpath=CLCALC_XPATH + "/cruiseAltitude",
+    gui=True,
+    gui_name="Altitude",
+    gui_group="Cruise",
 )
 
-# ----- Output -----
+cpacs_inout.add_input(
+    var_name="net_force",
+    var_type=float,
+    default_value=2000,
+    unit="1",  # AGGIUNGERE UNITA DI MISURA
+    descr="Engine net force",
+    xpath=CLCALC_XPATH + "/loadFactor",
+    gui=True,
+    gui_name="Load Factor",
+    gui_group="Cruise",
+)
+
+
+# ===== Output =====
 
 cpacs_inout.add_output(
-    var_name="output",
+    var_name="target_cl",
     default_value=None,
     unit="1",
-    descr="Description of the output",
-    xpath=CEASIOMPY_XPATH + "/test/myOutput",
+    descr="Value of CL to achieve to have a level flight with the given conditions",
+    xpath=SU2_TARGET_CL_XPATH,
+)
+
+cpacs_inout.add_output(
+    var_name="fixed_cl",
+    default_value=None,
+    unit="-",
+    descr="FIXED_CL_MODE parameter for SU2",
+    xpath=SU2_FIXED_CL_XPATH,
 )
